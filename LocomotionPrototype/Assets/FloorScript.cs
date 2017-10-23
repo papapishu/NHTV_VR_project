c0f68efab3d3d73fb0b0b752daca3d332396483a
Olgu <olgusahin@gmail.com>
2017-10-23 22:07:55 +0200

    initial dump

---
+++ LocomotionPrototype/Assets/FloorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour {
+	public GameObject target;
+	public float offset;
+	public float capsuleCenter;
+
+	void Start () {
+	}
+
+	void Update(){
+		capsuleCenter = target.GetComponent<CapsuleCollider> ().center.y;
+
+
+		//gameObject.transform.position = new Vector3(target.transform.position.x, (capsuleCenter/2) - offset, target.transform.position.z);
+	}
+}
---
+++ LocomotionPrototype/Assets/ResetPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour {
+
+	public GameObject spawnPoint;
+
+	
+	void Update () {
+		if (gameObject.transform.position.y < -10) {
+			transform.position = spawnPoint.transform.position;
+		}
+	}
+}
